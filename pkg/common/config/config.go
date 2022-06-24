d70f6985c3fd8a5490641d4ad6e7059af2aa56bd
aydina19 <aydina19@itu.edu.tr>
2022-06-24 11:45:20 +0300

    add config

---
+++ pkg/common/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Port  string `mapstructure:"PORT"`
+	DBUrl string `mapstructure:"DB_URL"`
+	KEY   string `mapstructure:"KEY"`
+}
+
+func LoadConfig() (c Config, err error) {
+	viper.AddConfigPath(".pkg/common/envs")
+	viper.SetConfigName("dev")
+	viper.SetConfigFile("env")
+
+	viper.AutomaticEnv()
+
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&c)
+
+	return
+}
