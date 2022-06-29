e40e062bde64b03162bf34b8bda15cefed68829c
AlperenAydın <55955068+aydnalperen@users.noreply.github.com>
2022-06-29 22:49:37 +0300

    add registeration

--- pkg/common/models/user.go
+++ pkg/common/models/user.go
@@ -2,12 +2,30 @@ package models
 
 import "gorm.io/gorm"
 
+type User_no_gorm struct {
+	email        string
+	username     string
+	passwordhash string
+	firstname    string
+	lastname     string
+	role         string
+}
 type User struct {
 	gorm.Model
 	email        string
 	username     string
 	passwordhash string
-	name         string
+	firstname    string
 	lastname     string
 	role         string
+}
+
+func (user *User) PairBody(u *User_no_gorm) {
+
+	user.email = u.email
+	user.username = u.username
+	user.passwordhash = u.passwordhash
+	user.firstname = u.firstname
+	user.lastname = u.lastname
+	user.role = u.role
 }
--- pkg/controller/register.go
+++ pkg/controller/register.go
@@ -1,7 +1,25 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"app/pkg/common/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) Registeration(ctx *gin.Context) {
+	body := models.User_no_gorm{}
 
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	var user models.User
+	user.PairBody(&body)
+
+	if result := h.DB.Create(&user); result != nil {
+		ctx.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+	ctx.JSON(http.StatusCreated, &user)
 }
