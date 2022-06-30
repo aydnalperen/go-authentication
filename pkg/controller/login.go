5a204137f6adc457ec60d077a296ca1cc4b00fc5
AlperenAydın <55955068+aydnalperen@users.noreply.github.com>
2022-06-30 23:56:21 +0300

    started to login

--- pkg/controller/login.go
+++ pkg/controller/login.go
@@ -1,7 +1,19 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"app/pkg/common/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) Login(ctx *gin.Context) {
+	body := models.User_no_gorm{}
 
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	var user models.User
+	user.PairBody(&body) // wrong
 }
