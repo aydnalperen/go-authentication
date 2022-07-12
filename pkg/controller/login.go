cf3d50628e2b4ad89dd75dbe8994aad3ef447521
AlperenAydın <55955068+aydnalperen@users.noreply.github.com>
2022-07-12 23:34:00 +0300

    fixes

--- pkg/controller/login.go
+++ pkg/controller/login.go
@@ -9,21 +9,21 @@ import (
 )
 
 func (h *Handler) Login(ctx *gin.Context) {
-	body := models.User_login{}
+	var body = models.User_login{}
 
 	if err := ctx.BindJSON(&body); err != nil {
 		ctx.AbortWithError(http.StatusBadRequest, err)
 		return
 	}
 
-	var user = models.User{}
+	var user models.User
 	result := h.DB.First(&user, "email=?", body.email)
-	if result != nil {
+	if result.Error != nil {
 		ctx.AbortWithError(http.StatusNotFound, result.Error)
 		return
 	}
 	hashedInput, _ := hash.HashPassword(body.password)
-	err := hash.CompareHashAndPassword(hashedInput, result.passwordhash)
+	err := hash.CheckPasswordHash(hashedInput, user.passwordhash)
 	if err != nil {
 		ctx.AbortWithError(http.StatusUnauthorized, err)
 		return
